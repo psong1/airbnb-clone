c191b67de0107b17077ef0189db67fbb71015848
345033730 <wentingou530@gmail.com>
2025-07-14 12:21:55 -0400

    update notification part in index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,18 +6,15 @@ const userRoutes = require("./api/user-routes");
 const bookingRoutes = require("./api/booking-routes");
 const reviewRoutes = require("./api/review-routes");
 const imageRoutes = require("./api/image-routes");
+const notificationRoutes = require('./api/notification-routes');
 
-console.log("Loading routes...");
+
 router.use("/api", listingRoutes);
-console.log("Listing routes loaded");
 router.use("/api", userRoutes);
-console.log("User routes loaded");
 router.use("/api", bookingRoutes);
-console.log("Booking routes loaded");
 router.use("/api", reviewRoutes);
-console.log("Review routes loaded");
 router.use("/api", imageRoutes);
-console.log("Image routes loaded");
+router.use('/api/notifications', notificationRoutes);
 
 router.get("/", (req, res) => {
   res.json({ message: "Welcome to Airbnb Clone API!" });
